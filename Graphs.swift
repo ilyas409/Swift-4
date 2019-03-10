bd14237ca7fea45a3a717bcf284e64452bd3fa65
Ilyas <ilyas1401408@gmail.com>
2019-03-10 09:59:01 -0700

    Update Graphs.swift

--- Graphs.swift
+++ Graphs.swift
@@ -1,4 +1,6 @@
 import Foundation
+
+// Adjacency list using a Dictionary with Strings as keys and arrays of Strings as values:
 let graph = ["A" : ["B", "E"],
              "B" : ["C"],
              "C" : ["Z"],
@@ -18,5 +20,15 @@ func findPath(from origin: String, to destination: String) ->Bool{
     }
     return false
 }
-
 findPath(from: "A", to: "Z")
+
+
+// Adjacency list using a Dictionary with Strings as keys and Dictionaries as values.
+// Each Dictionary has the node as its key and the a the weight as its value
+
+let graph = ["A" : [["B" : 4], ["E" : 10]],
+             "B" : [["C" : 21]],
+             "C" : [["Z" : 4]],
+             "D" : [["C" : 5]],
+             "E" : [["D" : 5], ["F" : 8]],
+             "F" : [["C" : 12]]]
